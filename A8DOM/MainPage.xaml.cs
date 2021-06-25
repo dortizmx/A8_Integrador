e48d7539f90d32d082dbf308610e51d483365a71
dortizmx <dortizmx@gmail.com>
2021-06-24 22:16:16 -0500

    * .gitignore: first commit
    * A8DOM.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * A8DOM.csproj:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * A8DOM.iOS.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * A8DOM.Android.csproj:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * icon.png:
    * icon.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon20.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon80.png:
    * Icon76.png:
    * icon_round.xml:
    * Icon152.png:
    * Icon180.png:
    * Icon167.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ A8DOM/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Data;
+
+
+namespace A8DOM
+{
+    public partial class MainPage : ContentPage
+    {
+        private int status_actual = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Click_Numero(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            string intnumero = boton.Text;
+            screen.Text += intnumero;
+        }
+
+        void Click_Limpia(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            screen.Text = "";
+        }
+
+        void Click_Resultado(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            try
+            {
+                var formula = screen.Text;
+                double result = Convert.ToDouble(new DataTable().Compute(formula, null));
+                screen.Text = "";
+                screen.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                screen.Text = ex.Message;
+            }
+            
+        }
+
+        void Click_Operador(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            var operador = boton.Text;
+            screen.Text += operador;
+        }
+
+        async Task<int> Calcula(string cadena)
+        {
+            
+
+            
+
+            return 1;
+        }
+
+    }
+}
